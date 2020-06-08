9fdc0a37aa0d7199cca28b3c5dc1406da14d76f7
hedongcheng09 <hedongcheng@enjoyk.cn>
2020-06-08 17:02:02 +0800

    edit

--- screenshot/Main.py
+++ screenshot/Main.py
@@ -1,7 +1,7 @@
 import shutil
 import time
 
-from flask import Flask
+from flask import Flask, request
 from flask import send_file
 
 from screenshot import BigBigWork, LoggingUtil
@@ -9,7 +9,6 @@ from screenshot import BigBigWork, LoggingUtil
 app = Flask(__name__, static_url_path='')
 
 logging = LoggingUtil.get_logging()
-file_name = ''
 
 
 @app.route('/screenshot/prod')
@@ -17,8 +16,8 @@ def screenshot_prod():
     try:
         rs1 = BigBigWork.normal_user_test('prod')
         rs2 = BigBigWork.vip_user_test('prod')
-        rs = {'normal_user': rs1, 'vip_user': rs2}
-        zip_compress('prod', 'prod')
+        file_name = zip_compress('prod', 'prod')
+        rs = {'normal_user': rs1, 'vip_user': rs2, 'file_name': file_name}
         shutil.rmtree('prod')
         return rs
     except Exception as e:
@@ -31,8 +30,8 @@ def screenshot_test():
     try:
         rs1 = BigBigWork.normal_user_test('test')
         rs2 = BigBigWork.vip_user_test('test')
-        rs = {'normal_user': rs1, 'vip_user': rs2}
-        zip_compress('test', 'test')
+        file_name = zip_compress('test', 'test')
+        rs = {'normal_user': rs1, 'vip_user': rs2, 'file_name': file_name}
         shutil.rmtree('test')
         return rs
     except Exception as e:
@@ -41,28 +40,26 @@ def screenshot_test():
 
 
 def date():
-    time.strftime('%Y-%m-%d')
+    return str(time.strftime('%Y%m%d%H%M'))
 
 
 def zip_compress(dir_name, output_filename):
-    global file_name
-    shutil.rmtree(file_name + '.zip', True)
     output_filename += date()
-    file_name = output_filename
     zip_file = shutil.make_archive(output_filename, 'zip', dir_name)
-    print(zip_file)
+    return zip_file.split('/')[-1]
 
 
 @app.route('/download/prod')
 def download_prod():
-    return send_file(file_name + '.zip')
+    name = request.args.get("name")
+    return send_file(name)
 
 
 @app.route('/download/test')
 def download_test():
-    return send_file(file_name + '.zip')
+    name = request.args.get("name")
+    return send_file(name)
 
 
 if __name__ == "__main__":
-    # zip_compress('prod', 'prod')
     app.run(host='0.0.0.0')
