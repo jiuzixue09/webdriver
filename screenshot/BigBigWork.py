16c22389ea56e8284af89f879bcc6aeb67f6367f
hedongcheng09 <hedongcheng@enjoyk.cn>
2020-06-11 16:40:53 +0800

    edit

--- screenshot/BigBigWork.py
+++ screenshot/BigBigWork.py
@@ -5,6 +5,9 @@ from time import sleep
 import yaml
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
 
 from screenshot import LoggingUtil
 
@@ -16,6 +19,14 @@ with open('config.yaml') as file:
 
 
 class BigBigWork:
+
+    def wait_for_elements(self, css_select, timeout=10):
+        try:
+            WebDriverWait(self.driver, timeout).until(EC.visibility_of_element_located((By.CSS_SELECTOR, css_select)))
+            return True
+        except Exception as e:
+            logging.error('can\'t find elements:{}'.format(css_select), e)
+            return False
 
     def __init__(self, headless=False, root=None, change_user_state_url=None, home_url=None, login_url=None, env=None):
 
@@ -72,10 +83,12 @@ class BigBigWork:
         time.sleep(1)
         if self.driver.current_url != self.home_url:
             try:
-                txt = self.driver.find_element_by_css_selector('span._tip').text
-                if len(txt) > 0:
-                    logging.error('%s', txt)
-                    return txt
+                if self.wait_for_elements('span._tip', 5):
+                    txt = self.driver.find_element_by_css_selector('span._tip').text
+                    if len(txt) > 0:
+                        logging.error('%s', txt)
+                        self.save(self.path + '/' + str(times) + '登_失败' + '.png')
+                        return txt
             except Exception as e:
                 logging.info('login error', e)
         return None
@@ -86,6 +99,7 @@ class BigBigWork:
     def screen_shot(self, times=0):
         try:
             logging.info("截图，图片目录=%s", self.path)
+            self.wait_for_elements('div.gift-content')
             gift = self.driver.find_element_by_css_selector('div.gift-content')
             sleep(1)
             self.save(self.path + '/' + str(times) + '登_gift' + '.png')
@@ -106,13 +120,8 @@ class BigBigWork:
             self.open(self.home_url)
             sleep(1)
 
-            for _ in range(3):
-                try:
-                    self.driver.find_element_by_id("p-button").click()
-                    break
-                except:
-                    sleep(1)
-
+            self.wait_for_elements('#p-button')
+            self.driver.find_element_by_id("p-button").click()
             items = self.driver.find_elements_by_css_selector('#VIPSelect > li.item')
 
             for i in range(len(items)):
@@ -183,6 +192,8 @@ def normal_user_test(env):
             big_big_work.delete_all_cookies()
             path = big_big_work.get_path()
     except Exception as e:
+        rs['status'] = 'error'
+        rs['reason'] = '页面加载异常'
         logging.error('normal user screenshot error', e)
 
     finally:
@@ -224,6 +235,8 @@ def vip_user_test(env):
     except Exception as e:
         logging.error('vip user screenshot error', e)
     finally:
+        rs['status'] = 'error'
+        rs['reason'] = '页面加载异常'
         big_big_work.close()
 
     rs['user_path'] = path
